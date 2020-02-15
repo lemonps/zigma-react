8b901b8bd266ad78852ac60b4c5efe0c53ad824b
Norasecure <elesrubsoap_4822@hotmail.com>
2020-02-16 01:10:18 +0700

    idn

--- src/components/Bid.js
+++ src/components/Bid.js
@@ -1,23 +1,19 @@
-<<<<<<< Updated upstream
 import React, { Component } from "react";
-=======
-import React, { useState } from "react";
 import { abi, address } from "../config";
 import Web3 from "web3";
->>>>>>> Stashed changes
 import { Panel, Row, Col, Grid, Dropdown, Icon } from "rsuite";
 
 class Bid extends Component {
   constructor(props) {
     super(props);
-    this.state = { txId: "" };
+    this.state = { txId: "", Interest: "" };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    this.setState({ txId: event.target.value });
+    this.setState({ Interest: event.target.value });
   }
 
   handleSubmit(event) {
@@ -29,22 +25,14 @@ class Bid extends Component {
     return (
       <form onSubmit={this.handleSubmit}>
         <label>
-<<<<<<< Updated upstream
-          Pick your favorite flavor:
-          <select value={this.state.txId} onChange={this.handleChange}>
-            <option value="grapefruit">Grapefruit</option>
-            <option value="lime">Lime</option>
-            <option value="coconut">Coconut</option>
-            <option value="mango">Mango</option>
-          </select>
-=======
           Interest:
           <input
             type="text"
-            value={Interest}
-            onChange={inputObj => setInterest(inputObj.target.value)}
+            value={this.state.Interest}
+            onChange={inputObj =>
+              this.state.handleChange(inputObj.target.value)
+            }
           />
->>>>>>> Stashed changes
         </label>
         <input type="submit" value="Submit" />
       </form>

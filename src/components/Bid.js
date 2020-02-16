efd544a08b17c74c09b1eb544bd5214f7e1e569f
Pattharathon Srithundorn <pattharathon.srithundorn@mail.kmutt.ac.th>
2020-02-16 08:11:15 +0700

    Auto stash before merge of "master" and "origin/master"

--- src/components/Bid.js
+++ src/components/Bid.js
@@ -55,6 +55,7 @@ const BorrowerRequestCard = props => {
   );
 };
 
+class Bid extends Component {
   async loadBlockChainData() {
     const web3 = new Web3();
     web3.setProvider(new web3.providers.HttpProvider("http://127.0.0.1:7545"));
